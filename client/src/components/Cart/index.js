241c9c5b3514aa0307a1eaac6de5687d03715973
Jonathan <eimerarts@gmail.com>
2020-10-25 16:40:19 -0500

    lesson 4

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,8 +5,15 @@ import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 const Cart = () => {
 const [state, dispatch] = useStoreContext();
+const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT)
+
 useEffect(() => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
@@ -17,7 +24,7 @@ useEffect(() => {
       getCart();
     }
   }, [state.cart.length, dispatch]);
-  
+
 function toggleCart() {
   dispatch({ type: TOGGLE_CART });
 }
@@ -28,6 +35,25 @@ function calculateTotal() {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+  function submitCheckout() {
+    const productIds = [];
+  
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+    getCheckout({
+        variables: { products: productIds }
+      });
+      useEffect(() => {
+        if (data) {
+          stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+        }
+      }, [data]); 
   }
 
 if (!state.cartOpen) {
@@ -53,9 +79,9 @@ if (!state.cartOpen) {
         <strong>Total: ${calculateTotal()}</strong>
         {
           Auth.loggedIn() ?
-            <button>
-              Checkout
-            </button>
+          <button onClick={submitCheckout}>
+          Checkout
+        </button>
             :
             <span>(log in to check out)</span>
         }
